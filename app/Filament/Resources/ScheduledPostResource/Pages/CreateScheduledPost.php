27ff6782ea28e9a8fe57e37df0f14dbc525668b1
hainc <haidev306@gmail.com>
2026-03-14 08:38:41 +0700

    fix: auto-fill ScheduledPost form when redirected from PostTemplate 'Dùng ngay'

--- app/Filament/Resources/ScheduledPostResource/Pages/CreateScheduledPost.php
+++ app/Filament/Resources/ScheduledPostResource/Pages/CreateScheduledPost.php
@@ -3,9 +3,37 @@
 namespace App\Filament\Resources\ScheduledPostResource\Pages;
 
 use App\Filament\Resources\ScheduledPostResource;
+use App\Models\PostTemplate;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateScheduledPost extends CreateRecord
 {
     protected static string $resource = ScheduledPostResource::class;
+
+    public function mount(): void
+    {
+        parent::mount();
+
+        $templateId = request()->query('template_id');
+        if ($templateId) {
+            $template = PostTemplate::find($templateId);
+            if ($template) {
+                $this->form->fill([
+                    'content' => $template->content,
+                    'images' => $template->images ?? [],
+                    'settings' => [
+                        'minDelay' => 30,
+                        'maxDelay' => 120,
+                        'spinEnabled' => true,
+                        'seedLike' => true,
+                        'seedComments' => $template->seed_comments
+                            ? collect($template->seed_comments)->pluck('text')->filter()->values()->toArray()
+                            : [],
+                        'seedDelay' => 10,
+                    ],
+                ]);
+                $template->incrementUsage();
+            }
+        }
+    }
 }
